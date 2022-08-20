66af905a8e3e570ad12cf82726df8ebb2ef3e047
Eugene Pobochny <eugene.pobochny@gmail.com>
2022-08-20 22:25:29 +0300

    v0.0.11

--- src/cmd/global.ts
+++ src/cmd/global.ts
@@ -2,7 +2,7 @@ import { Command } from "commander";
 
 export const program = new Command("webkraken")
     .description("web crawler")
-    .version("0.0.10", "-v --version")
+    .version("0.0.11", "-v --version")
     .option("--verbose", "verbose output");
 
 export interface GlobalOptions {
